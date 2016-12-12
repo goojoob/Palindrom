f973ff74920b3c7143deef9ef45f82d159d01c76
Antonio <antrorc@gmail.com>
2016-12-12 08:29:35 +0100

    First Commit
    Example to test if a sentence is Palindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Input text to test if is Palindrom:");
+            text = Console.ReadLine();
+
+            Word wordToTest = new Word(text);
+
+            if (wordToTest.isPalindrom())
+            {
+                Console.WriteLine("\n{0} is Palindrom!",text);
+            }
+            else
+            {
+                Console.WriteLine("\n{0} is not Palindrom :(", text);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Palindrom/Word.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    class Word
+    {
+        string word;
+
+        public Word(string text)
+        {
+            word = removeCaseSensitive(removeBlanks(text));
+        }
+
+        private string removeBlanks(string text)
+        {
+            return text.Replace(" ", "");
+        }
+
+        private string removeCaseSensitive(string text)
+        {
+            return text.ToUpper();
+        }
+
+
+        public bool isPalindrom()
+        {
+            int endPosition = word.Length - 1;
+            int initPosition = 0;
+
+            while (initPosition < endPosition)
+            {
+                if (word[initPosition] != word[endPosition])
+                {
+                    return false;
+                }
+                initPosition++;
+                endPosition--;
+            }
+
+            return true;
+        }
+    }
+}
